dd3065f4c77a5e2f0b697370721701d25318c5cc
swapnil <sutarswapnil322@gmail.com>
2024-12-20 12:05:57 +0530

    import Swagger config and create route for documentation

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { swaggerServe, swaggerSetup } from "./Config/Swagger.config.js";
 
 const app = express();
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:4000",
-  ],
+  origin: ["http://localhost:5173", "http://localhost:4000"],
   methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 };
-
 
 app.use(cookieParser());
 app.use(express.json());
@@ -24,6 +21,8 @@ import Task from "./Routes/Task.routes.js";
 
 app.use("/api/v1/auth", Auth);
 app.use("/api/v1/Task", Task);
+
+app.use("/api-docs", swaggerServe, swaggerSetup);
 
 app.get("/", (req, res) => {
   return res.json({
